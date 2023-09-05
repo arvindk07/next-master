dcf98da49d1e7fbc2d09a4109b1ae19fd36e3a75
arvindk07 <info.arvind07@gmail.com>
2023-09-05 08:54:47 +0530

    dynamic metadata

---
+++ app/about/page.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Next SEO About Page</title>
+        <meta name="discriptions" content="content" />
+      </Head>
+      <h1>Hello</h1>
+    </>
+  );
+};
+
+export default About;
---
+++ app/contact/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const metadata = {
+  title: "contact | Page",
+  description: "Contact Page",
+};
+
+const Contacts = () => {
+  return <div>Contacts</div>;
+};
+
+export default Contacts;
